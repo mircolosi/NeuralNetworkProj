ad0ea165e0096402c8f1f24c0ae861ec799da562
mircolosi <bigprof@hotmail.it>
2016-10-23 16:47:57 +0200

    fine prima passata ccodice nuovo

--- MATLAB/new_born.m
+++ MATLAB/new_born.m
@@ -72,7 +72,7 @@ if (convert_txt == true)
         rmdir(zip_folder, 's');
     end
     mkdir(zip_folder);
-
+    
     
     for i = 1:length(authors_folders)
         author = authors_folders(i).name;
@@ -127,12 +127,132 @@ author_text_folders = author_text_folders(3:end);
 % kolmogorov distance.
 
 SM = zeros(n_author);
-
+disp('Starting computation similarity matrix!');
 
 n = 1;
 achepuntosiamo=1;
 
-disp('Starting computation similarity matrix!');
+for i = 1:length(list_name_files)
+    
+    
+    [current_folder,a,~] = fileparts(list_name_files{i});
+    [~,author,~] = fileparts(current_folder)
+    disp(['Converting files between author ', author]);
+    
+    fid_1 = fopen(list_name_files{i});
+    text1 = fread(fid_1, Inf, '*char');
+    fclose(fid_1);
+    
+    for j = i:length(list_name_files)
+        
+        [other_folder,b,~] = fileparts(list_name_files{j});
+        [~,other_author,~] = fileparts(other_folder);
+        
+        folder_cat = fullfile(midi_text, strcat(author,'_',other_author));
+        % folder for the concatenation of the two file in invers
+        % order
+        inv_folder_cat = fullfile(midi_text, strcat(other_author,'_',author));
+        
+        % folder that stores the concatenation of the two file in direct
+        % order
+        zip_cat = fullfile(zip_folder, strcat(author,'_',other_author));
+        % folder that stores the concatenation of the two file in inverse
+        % order
+        inv_zip_cat = fullfile(zip_folder, strcat(other_author,'_',author));
+        
+        if ~exist(folder_cat, 'dir')
+            mkdir(folder_cat);
+        end
+        if ~exist(inv_folder_cat, 'dir')
+            mkdir(inv_folder_cat);
+        end
+        if ~exist(zip_cat, 'dir')
+            mkdir(zip_cat);
+        end
+        if ~exist(inv_zip_cat, 'dir')
+            mkdir(inv_zip_cat);
+        end
+        
+        fid_2 = fopen(list_name_files{j});
+        text2 = fread(fid_2, Inf, '*char');
+        fclose(fid_2);
+        
+        if (~exist(fullfile(folder_cat, strcat(a, '_', b, '.txt')),'file') || ...
+                ~exist(fullfile(inv_folder_cat, strcat(b, '_', a, '.txt')),'file'))
+            tmp_txt_file1 = fullfile(folder_cat, strcat(a, '_', b, '.txt'));
+            fid_cat = fopen(tmp_txt_file1, 'w+');
+            textcat = vertcat(text1,text2);
+            fprintf(fid_cat, '%c', textcat);
+            fclose(fid_cat);
+            tmp_txt_file2 = fullfile(inv_folder_cat, strcat(b, '_', a, '.txt'));
+            inv_fid_cat = fopen(tmp_txt_file2,'w+');
+            inv_textcat = vertcat(text2,text1);
+            fprintf(inv_fid_cat, '%c', inv_textcat);
+            fclose(inv_fid_cat);
+        end
+        
+        % zip the two files
+        zip_file1 = fullfile(zip_folder, author,strcat(a, '.zip'));
+        
+        if(~exist(zip_file1,'file'))
+            zip(zip_file1, fullfile(current_folder, strcat(a, '.txt')));
+        end
+        
+        zip_file2 = fullfile(zip_folder, other_author, strcat(b, '.zip'));
+        if (~exist(zip_file2, 'file'))
+            zip(zip_file2, fullfile(other_folder, strcat(b, '.txt')));
+        end
+        
+        % zip the direct and inverse concatenated files
+        zip_file12 = fullfile(zip_cat, strcat(a, '_', b, '.zip'));
+        zip_file21 = fullfile(inv_zip_cat, strcat(b, '_', a, '.zip'));
+        
+        if (~exist(zip_file12,'file') || ~exist(zip_file21,'file'))
+            zip(zip_file12, fullfile(folder_cat, strcat(a, '_', b, '.txt')));
+            
+            zip(zip_file21, fullfile(inv_folder_cat, strcat(b, '_', a, '.txt')));
+            
+        end
+        
+        % calulate the lenght of the zipped files
+        
+        fid1 = fopen(zip_file1);
+        fid2 = fopen(zip_file2);
+        fid12 = fopen(zip_file12);
+        fid21 = fopen(zip_file21);
+        
+        c_file1 = fread(fid1, Inf, '*char');
+        c_file2 = fread(fid2, Inf, '*char');
+        c_file12 = fread(fid12, Inf, '*char');
+        c_file21 = fread(fid21, Inf, '*char');
+        
+        K1 = length(c_file1);
+        K2 = length(c_file2);
+        K12 = length(c_file12);
+        K21 = length(c_file21);
+        
+        fclose(fid1);
+        fclose(fid2);
+        fclose(fid12);
+        fclose(fid21);
+        % caluclate the distance based on the eximation of
+        % Kolmogorov complexity
+        ds_12 = (K12 - K2 + K21 - K1)/K12;
+        ds_21 = (K21 - K1 + K12 - K2)/K21;
+        
+        % fill the Similiarity matrix with the distances
+        SM(i,j) = ds_12;
+        SM(j,i) = ds_21;
+        
+        delete(tmp_txt_file2);
+        delete(tmp_txt_file1);
+        
+        achepuntosiamo = achepuntosiamo+1
+    end
+    
+end
+
+return
 for i = 1:length(author_text_folders)
     % author: the name of the current author
     author = author_text_folders(i).name;
