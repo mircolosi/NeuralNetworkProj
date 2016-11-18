a773cc92152d6bbcfc16e12d0a2abd1facf877d2
root <root@istin-K53SJ>
2016-11-18 20:34:56 +0100

    KNN a mano - basi

--- MATLAB/kNNSimilaritySearch.m
+++ MATLAB/kNNSimilaritySearch.m
@@ -1,7 +1,24 @@
-function distance  = kNNSimilaritySearch( new_instance, distance_matrix )
-%UNTITLED4 Summary of this function goes here
-%   Detailed explanation goes here
+function [d_vec, s_vec]  = kNNSimilaritySearch( new_instance, data_points_vec, distance_matrix, k )
 
+if length(new_instance ~= 2)
+    disp('Error, bad instance');
+end
 
+ni_name = new_instance(2);
+dp_names = data_points_vec(:,2);
+
+bool_idx = strcmp(ni_name,dp_names);
+if (any(bool_idx)) % already in the datapoints
+    ni_idx = find(bool_idx);
+    [d_vec, sort_idx] = sort(distance_matrix(ni_idx,:));
+    s_vec = dp_names(sort_idx);
+else
+    % PLACEHOLDER
+    % compute the distances between this file and all the others.
+    % since we have the abs path of all files, this can be done
+    % pretty easily and with not so much time.
+end
+    d_vec = d_vec(1:k);
+    s_vec = s_vec(1:k);
 end
 
--- MATLAB/knn_IRVIN.m
+++ MATLAB/knn_IRVIN.m
@@ -13,6 +13,7 @@ authors_folders = authors_folders(3:end);
 dataset_size = 0;
 X = {};
 Y = {};
+N = {};
 for i = 1:length(authors_folders)
     author_files = dir(fullfile(midi_folder,authors_folders(i).name));
     author_files = author_files(3:end);
@@ -20,9 +21,11 @@ for i = 1:length(authors_folders)
     for j = 1:length(author_files)
         X(i, j) = {fullfile(midi_folder,authors_folders(i).name,author_files(j).name)};
         Y(i, j) = {authors_folders(i).name};
+        N(i, j) = {[authors_folders(i).name(1),authors_folders(i).name(end),'_',int2str(j)]};
         dataset_size = dataset_size + 1;
     end
 end
 
 X = reshape(X',dataset_size,1);
+X = horzcat(X,reshape(N',dataset_size,1));
 Y = reshape(Y',dataset_size,1);
